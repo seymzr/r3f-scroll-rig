7d0a57424dbd2289fd8c8614fab193b155af97b7
seymzr <seymzr@gmail.com>
2024-03-09 10:52:17 +0400

    s

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -17,7 +17,7 @@ export const LoadingScreen = (props) => {
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-1000 pointer-events-none
-  flex items-center justify-center bg-indigo-50 
+  flex items-center justify-center bg-black 
   ${started ? "opacity-0" : "opacity-100"}`}
     >
       <div className="text-4xl md:text-9xl font-bold text-indigo-900 relative">
@@ -27,9 +27,9 @@ export const LoadingScreen = (props) => {
             width: `${progress}%`,
           }}
         >
-          Wawa Sensei
+          Sabah.hub
         </div>
-        <div className="opacity-40">Wawa Sensei</div>
+        <div className="opacity-40">Sabah.hub</div>
       </div>
     </div>
   );
