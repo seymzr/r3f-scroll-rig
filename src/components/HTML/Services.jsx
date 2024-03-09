5930b01c981b77f82b5e964500b9f997983f7961
seymzr <seymzr@gmail.com>
2024-03-09 13:34:55 +0400

    s

--- src/components/HTML/Services.jsx
+++ src/components/HTML/Services.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='services'>
-    <h2>
+    <div className='text-white flex items-center flex-col  container mx-auto'>
+    <h2 className='text-7xl font-normal'>
       <span style={{ color: "orange" }}>Notable</span> Startups
     </h2>
-    <div>
-      <ul>
-        <li>Lorem, ipsum</li>
-        <li>Lorem, ipsum</li>
-        <li id='biggerLi'>
+    <div className='flex items-center justify-around w-full py-8'>
+      <ul className='list-style-none'>
+        <li className='-ml-8 opacity-50 text-2xl m-4'>Lorem, ipsum</li>
+        <li className='-ml-4 opacity-75 text-2xl m-4'>Lorem, ipsum</li>
+        <li className='text-4xl bg-[#dcdcdc4f] py-6 px-8 rounded-[10px]' >
           Lorem, ipsum
-          <br /> <span>lorem ipsum dolor sit amet</span>
+          <br /> <span className='text-xl text-[#b4b4b4]'>lorem ipsum dolor sit amet</span>
         </li>
-        <li>Lorem, ipsum</li>
-        <li>Lorem, ipsum</li>
+        <li className='-ml-4 opacity-75 text-2xl m-4'>Lorem, ipsum</li>
+        <li className='-ml-8 opacity-50 text-2xl m-4'>Lorem, ipsum</li>
       </ul>
       <div></div>
     </div>
