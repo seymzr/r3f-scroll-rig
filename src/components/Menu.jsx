50395f5b1ce3fb086ad35f985112ff4196126ba8
seymzr <seymzr@gmail.com>
2024-03-10 19:33:20 +0400

    s

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,9 +3,12 @@ import facebook from '../assets/facebook.svg'
 import instagram from '../assets/instagram.svg'
 import linkedin from '../assets/linkedin.svg'
 import newsletter from '../assets/newsletter.svg'
+import rightarrow from '../assets/rightarrow.svg'
+import { useState } from 'react'
 export const Menu = (props) => {
   const { onSectionChange, menuOpened, setMenuOpened } = props
-
+  const [companyClicked,setCompanyClicked] = useState()
+  const [aboutClicked,setAboutClicked] = useState()
   return (
     <div className="flex  w-full justify-between">
       <img className="z-50" src={logo} alt="sabahhub logo" />
@@ -30,36 +33,65 @@ export const Menu = (props) => {
             </a>
           </div>
           <div className="flex flex-col">
-            <h2 className="text-[#FF7B25] font-medium text-2xl mb-2">About</h2>
-            <a className="text-xl text-white py-1 font-normal" href="">
+          <div className='flex w-full items-center justify-between mb-2'>
+            <h2 className="text-[#FF7B25] font-medium text-2xl ">About</h2>
+            <button onClick={()=>{setAboutClicked(!aboutClicked)}} className='ml-12 md:hidden '><img className={`${aboutClicked?"-rotate-90":""} duration-300`}src={rightarrow} alt="rightarrow" /></button>
+            </div>
+            <div className={`${aboutClicked?"block":"hidden md:block"} duration-300 flex flex-col`}>
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Our Team
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Overview
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Our Portfolio
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Baku ID
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               About Us
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Blog
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Career
             </a>
-            <a className="text-xl text-white py-1 font-normal" href="">
+            <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
               Contact
             </a>
+            </div>
           </div>
+          <hr className='h-[1px] w-full my-2 border-0 bg-[#787878] block md:hidden' />
           <div className="flex flex-col">
-            <h2 className="text-[#FF7B25] font-medium text-2xl mb-2">Company</h2>
-            <div>
-              {}
+            <div className='flex w-full items-center justify-between mb-2'>
+            <h2 className="text-[#FF7B25] font-medium text-2xl ">Company</h2>
+            <button onClick={()=>{setCompanyClicked(!companyClicked)}} className='ml-12 md:hidden '><img className={`${companyClicked?"-rotate-90":""} duration-300`}src={rightarrow} alt="rightarrow" /></button>
+            </div>
+            <div className={`${companyClicked?"block":"hidden md:block"} duration-300 flex flex-col`}>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Privacy
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Support
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Help Desk
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                FAQ
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Blog
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Career
+              </a>
+              <a className="text-xl text-[#A5A5A5] md:text-white py-1 font-normal" href="">
+                Contact
+              </a>
             </div>
           </div>
           <div className="self-center">
@@ -80,21 +112,21 @@ export const Menu = (props) => {
                 <p className="text-lg text-white">57 Uzeyir Hajibeyov, Baku, Azerbaijan</p>
               </div>
               <div className="flex items-center">
-              <span className="font-bold text-white mr-2">E: </span>
-              <p className="text-lg text-white">info@sabahlab.edu.az</p>
+                <span className="font-bold text-white mr-2">E: </span>
+                <p className="text-lg text-white">info@sabahlab.edu.az</p>
               </div>
               <div className="flex items-center">
-              <span className="font-bold text-white mr-2 self-start">P: </span>{' '}
-              <p className="text-lg text-white">
-                Acceleration: + 994 (051) 240-16-97 <br />
-                Incubation: + 994 (051) 277-13-50
-              </p>
+                <span className="font-bold text-white mr-2 self-start">P: </span>{' '}
+                <p className="text-lg text-white">
+                  Acceleration: + 994 (051) 240-16-97 <br />
+                  Incubation: + 994 (051) 277-13-50
+                </p>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <p className=' loading text-lg text-white text-center'>&copy; 2024 SABAH.Hub. All rights reserved.</p>
+          <p className=" loading text-lg text-white text-center">&copy; 2024 SABAH.Hub. All rights reserved.</p>
         </div>
       </div>
     </div>
